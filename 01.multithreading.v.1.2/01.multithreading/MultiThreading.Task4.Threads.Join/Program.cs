cfc054c82c0d3b2243619342604f87d3059c0e02
Muhammad_Yousuf <Muhammad_Yousuf@EPPLKRAW0436>
2022-10-21 00:23:26 +0200

    commit task 4

--- 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -34,35 +34,54 @@ namespace MultiThreading.Task4.Threads.Join
 
             // feel free to add your code
             int numThreads = 10;
-            WaitCallback waitCallback = new WaitCallback(EnterSempahore);
-            ThreadPool.QueueUserWorkItem(waitCallback, numThreads);
+          //  WaitCallback waitCallback = new WaitCallback(EnterSempahore);
+           // ThreadPool.QueueUserWorkItem(waitCallback, numThreads);
             
             
+            while(numThreads >1)
+            {
+                Thread thread = new Thread(() => {
+                  
+                    Console.WriteLine("New Thread" + numThreads);
+                    numThreads--;
+
+                });
+                thread.Start();
+                thread.Join();
+            }
            
             Console.ReadLine();
         }
 
-      
+        //private static int printThread(int numThreads)
+        //{
+        //    numThreads=numThreads-1;
+        //   Console.WriteLine("New Thread" + numThreads);
+        //    return numThreads;
+        //}
 
-        private static void EnterSempahore(object i)
-        {
-            int x = Convert.ToInt32(i);
-            while(x>0)
-            {
-                Thread t1 = new Thread(threadProc);
-                t1.Start(x);
-                t1.Join();
-                x--;
-            }
+        //private static void EnterSempahore(object i)
+        //{
+        //    int x = Convert.ToInt32(i);
+        //    while(x>0)
+        //    {
+        //        Thread t1 = new Thread(threadProc);
+        //        t1.Start(x);
+        //        t1.Join();
+        //        x--;
+        //    }
           
-            semaphoreSlim.Wait();
-        }
+        //    semaphoreSlim.Wait();
+        //}
 
        
-        private static void threadProc(object state)
-        {
-            Console.WriteLine("New Thread" + state.ToString());
-        }
+        //private static void threadProc(object state)
+        //{
+        //    Console.WriteLine("New Thread" + state.ToString());
+        //}
+
+
+
 
 
     }
